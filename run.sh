be5ee10d96ce0527878e52f3af7064a5c5742ec9
JiTao <jitao@ruc.edu.cn>
2022-07-18 17:26:05 +0800

    readme

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 # for split data
-# nohup ./build/CellTree_split /data/jitao/dataset/OSM/osm.csv /data/jitao/dataset/OSM/split2/ > /data/jitao/dataset/OSM/split2.log 2>&1 &
-# nohup ./build/CellTree_split /data/jitao/dataset/Tiger/center_tiger_east_17m.txt /data/jitao/dataset/Tiger/split2/ > log/tiger_split2.log 2>&1 &
-# nohup ./build/CellTree_split /data/jitao/dataset/uniform/2d_len_1e8_seed_1.csv /data/jitao/dataset/uniform/split/ > /data/jitao/dataset/uniform/split.log 2>&1 &
-# nohup ./build/CellTree_split /data/jitao/dataset/skewed/2d_len_1e8_seed_1.csv /data/jitao/dataset/skewed/split/ > /data/jitao/dataset/skewed/split.log 2>&1 &
+# nohup ./build/CellTree_split /data/userpath/dataset/OSM/osm.csv /data/userpath/dataset/OSM/split2/ > /data/userpath/dataset/OSM/split2.log 2>&1 &
+# nohup ./build/CellTree_split /data/userpath/dataset/Tiger/center_tiger_east_17m.txt /data/userpath/dataset/Tiger/split2/ > log/tiger_split2.log 2>&1 &
+# nohup ./build/CellTree_split /data/userpath/dataset/uniform/2d_len_1e8_seed_1.csv /data/userpath/dataset/uniform/split/ > /data/userpath/dataset/uniform/split.log 2>&1 &
+# nohup ./build/CellTree_split /data/userpath/dataset/skewed/2d_len_1e8_seed_1.csv /data/userpath/dataset/skewed/split/ > /data/userpath/dataset/skewed/split.log 2>&1 &
 
 # for training 
-# nohup ./build/CellTree /data/jitao/dataset/OSM/osm.csv /data/jitao/dataset/OSM/trained_modelParam_for_split2/ > log/pointSearch_v1.3.log 2>&1 &
+# nohup ./build/CellTree /data/userpath/dataset/OSM/osm.csv /data/userpath/dataset/OSM/trained_modelParam_for_split2/ > log/pointSearch_v1.3.log 2>&1 &
 # change range search
-# nohup ./build/CellTree /data/jitao/dataset/OSM/osm.csv /data/jitao/dataset/OSM/new_trained_model_param_for_split2/ > log/rangeSearch_v1.2.log 2>&1 &
-# nohup ./build/CellTree /data/jitao/dataset/OSM/osm.csv /data/jitao/dataset/OSM/trained_modelParam_for_split2_largeBatch/ > log/rangeSearch_lbatch_v1.1.log 2>&1 &
-# nohup ./build/CellTree /data/jitao/dataset/OSM/osm.csv /data/jitao/dataset/OSM/trained_modelParam_for_split2_noac2/ > log/rangeSearch_noac2_v1.1.log 2>&1 &
+# nohup ./build/CellTree /data/userpath/dataset/OSM/osm.csv /data/userpath/dataset/OSM/new_trained_model_param_for_split2/ > log/rangeSearch_v1.2.log 2>&1 &
+# nohup ./build/CellTree /data/userpath/dataset/OSM/osm.csv /data/userpath/dataset/OSM/trained_modelParam_for_split2_largeBatch/ > log/rangeSearch_lbatch_v1.1.log 2>&1 &
+# nohup ./build/CellTree /data/userpath/dataset/OSM/osm.csv /data/userpath/dataset/OSM/trained_modelParam_for_split2_noac2/ > log/rangeSearch_noac2_v1.1.log 2>&1 &
 
 # * point search
 
